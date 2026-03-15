e885d37b326da272ec4cbf2e04fa3c183fc0abec
AgrimChauhan09 <agrimchauhan.18@gmail.com>
2026-03-15 12:33:08 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function fetchWindData({ startDate, endDate, horizon }) {
-  const response = await axios.get('/api/wind-data', {
+  const response = await axios.get(`${API_URL}/api/wind-data`, {
     params: {
       startDate,
       endDate,
       horizon,
     },
   });
+
   return response.data;
 }
